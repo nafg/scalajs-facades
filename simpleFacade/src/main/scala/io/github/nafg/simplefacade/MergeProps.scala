de86c1ad1ae63f43984325e37c78f5082c46d29a
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2021-12-20 01:28:22 -0500

    Refactor MergeProps

--- simpleFacade/src/main/scala/io/github/nafg/simplefacade/MergeProps.scala
+++ simpleFacade/src/main/scala/io/github/nafg/simplefacade/MergeProps.scala
@@ -10,10 +10,28 @@ private[simplefacade] object MergeProps {
   }
 
   private def AnyJsFunction(f: AnyJsFunction): AnyJsFunction = f
-  private val shouldMergeArray = (key: String) => key == "children"
-  private val shouldMergeObject = (key: String) => key == "style"
-  private val shouldMergeString = (key: String) => key == "className"
-  private val shouldMergeFunc = (key: String) => key.startsWith("on")
+  private def mergeChildrenArrays(value1: js.Any, value2: js.Any) =
+    if (value1.asInstanceOf[js.Array[js.Any]].length == 0) value2
+    else if (value2.asInstanceOf[js.Array[js.Any]].length == 0) value1
+    else
+      value1
+        .asInstanceOf[js.Array[js.Any]]
+        .concat(value2.asInstanceOf[js.Array[js.Any]])
+  private def mergeClassNames(value1: js.Any, value2: js.Any): js.Any =
+    if (value1.asInstanceOf[String] == "") value2
+    else if (value2.asInstanceOf[String] == "") value1
+    else value1.toString + " " + value2
+  private def mergeStyleObjects(value1: js.Any, value2: js.Any) =
+    js.Object.assign(
+      js.Object(),
+      value1.asInstanceOf[js.Object],
+      value2.asInstanceOf[js.Object]
+    )
+  private def mergeEventHandlers(value1: js.Any, value2: js.Any): js.ThisFunction =
+    AnyJsFunction { (_this, args) =>
+      value1.asInstanceOf[js.Function].call(_this, args: _*)
+      value2.asInstanceOf[js.Function].call(_this, args: _*)
+    }
   private def merge(key: String, value1: js.Any, value2: js.Any): js.Any =
     if (js.isUndefined(value1))
       value2
@@ -21,27 +39,14 @@ private[simplefacade] object MergeProps {
       value1
     else if (js.typeOf(value1) != js.typeOf(value2))
       value2
-    else if (js.typeOf(value1) == "array" && shouldMergeArray(key)) {
-      if (value1.asInstanceOf[js.Array[js.Any]].length == 0) value2
-      else if (value2.asInstanceOf[js.Array[js.Any]].length == 0) value1
-      else value1.asInstanceOf[js.Array[js.Any]].concat(value2.asInstanceOf[js.Array[js.Any]])
-    }
-    else if (js.typeOf(value1) == "string" && shouldMergeString(key)) {
-      if (value1.asInstanceOf[String] == "") value2
-      else if (value2.asInstanceOf[String] == "") value1
-      else value1.toString + " " + value2
-    }
-    else if (js.typeOf(value1) == "object" && shouldMergeObject(key))
-      js.Object.assign(
-        js.Object(),
-        value1.asInstanceOf[js.Object],
-        value2.asInstanceOf[js.Object]
-      )
-    else if (js.typeOf(value1) == "function" && shouldMergeFunc(key))
-      AnyJsFunction { (_this, args) =>
-        value1.asInstanceOf[js.Function].call(_this, args: _*)
-        value2.asInstanceOf[js.Function].call(_this, args: _*)
-      }
+    else if (js.typeOf(value2) == "array" && key == "children")
+      mergeChildrenArrays(value1, value2)
+    else if (js.typeOf(value2) == "string" && key == "className")
+      mergeClassNames(value1, value2)
+    else if (js.typeOf(value2) == "object" && key == "style")
+      mergeStyleObjects(value1, value2)
+    else if (js.typeOf(value2) == "function" && key.startsWith("on"))
+      mergeEventHandlers(value1, value2)
     else
       value2
   def apply(propsObjects: js.Array[js.Object]): js.Object =
