46745fc5774506d4b633d04c75b3b845723e7de8
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2021-12-20 01:16:43 -0500

    Add some tests

---
+++ simpleFacade/src/test/scala/io/github/nafg/simplefacade/Tests.scala
@@ -0,0 +1,28 @@
+package io.github.nafg.simplefacade
+
+import scala.scalajs.js
+import japgolly.scalajs.react.vdom.html_<^._
+
+
+class Tests extends munit.FunSuite {
+  test("TagMod setting") {
+    assertEquals(
+      Factory((), null).apply(VdomAttr[String]("a") := "x").rawProps.asInstanceOf[js.Dictionary[Any]].toMap,
+      Map("a" -> "x")
+    )
+  }
+  test("PropTypes settings") {
+    class MyProps extends PropTypes {
+      val a = of[Int]
+    }
+    assertEquals(
+      Factory(new MyProps, null).apply(_.a := 1).rawProps.asInstanceOf[js.Dictionary[Any]].toMap,
+      Map("a" -> 1)
+    )
+    assertEquals(
+      Factory(new MyProps, null).apply(List[(String, js.Any)]("a" -> 1, "b" -> true))
+        .rawProps.asInstanceOf[js.Dictionary[Any]].toMap,
+      Map[String, AnyVal]("a" -> 1, "b" -> true)
+    )
+  }
+}
