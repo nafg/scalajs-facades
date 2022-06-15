4e6c1fbbfc79b636813a95b9a3655070bc37ed44
Naftoli Gugenheim <naftoligug@gmail.com>
2022-06-14 20:43:45 -0400

    Help the compiler a bit

--- reactSelect/src/test/scala/io/github/nafg/scalajs/facades/reactselect/CompileTimeTests.scala
+++ reactSelect/src/test/scala/io/github/nafg/scalajs/facades/reactselect/CompileTimeTests.scala
@@ -4,15 +4,15 @@ import io.github.nafg.scalajs.facades.reactselect.SelectionType.Id
 
 
 object CompileTimeTests {
-  Creatable(List(Opt("hello"), Opt("goodbye")))("hello": Id[String])()(
+  Creatable[String](List(Opt("hello"), Opt("goodbye")))("hello": Id[String])()(
     _.isValidNewOption := ((_: String, _: Option[String], _: Seq[Opt[String]]) => true)
   )
 
-  Creatable(List(Opt("hello"), Opt("goodbye")))(Option.empty[String])()(
-      _.isValidNewOption := ((_: String, _: Option[String], _: Seq[Opt[String]]) => true)
+  Creatable[String](List(Opt("hello"), Opt("goodbye")))(Option.empty[String])()(
+    _.isValidNewOption := ((_: String, _: Option[String], _: Seq[Opt[String]]) => true)
   )
 
-  Creatable(List(Opt("hello"), Opt("goodbye")))(Seq("hello"))()(
+  Creatable[String](List(Opt("hello"), Opt("goodbye")))(Seq("hello"))()(
     _.isValidNewOption := ((_: String, _: Seq[String], _: Seq[Opt[String]]) => true)
   )
 }
